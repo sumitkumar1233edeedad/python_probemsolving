4e77500fb40e7ec5ca06ce4b947a70d53ff42955
sumitkumar1233edeedad <your-email@example.com>
2025-05-03 21:50:21 +0530

    new

---
+++ 14taketen.py
@@ -0,0 +1,3 @@
+m = ['s', 'e', 'w', 'n', 'n', 's', 'e', 'w', 'n', 's']
+walk =['s', 'e', 'w', 'n', 'n', 'e', 'e', 'w', 'n', 's']
+print(len(walk) == 10 and walk.count('n') == walk.count('s') and walk.count('e') == walk.count('w'))
